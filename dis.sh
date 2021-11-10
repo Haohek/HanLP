f4727bc1541a7060d19afe8e5d02827f8354c820
Haohek <94043582+Haohek@users.noreply.github.com>
2021-11-10 17:41:56 +0700

    Update and rename doc.sh to dis.sh

--- dis.sh
+++ dis.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+softwareupdate -l
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+brew install --cask screen
+brew install coreutils
 apt update
 apt upgrade -y
 apt install -y
